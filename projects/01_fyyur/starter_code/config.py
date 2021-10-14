d81a8ec2cc5201ebd6c8183633a82d116cc3dfa0
derekjreed <derekjreed@gmail.com>
2021-10-14 16:36:24 +0100

    Added Postgres URI and SQLAlc start up config

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -6,8 +6,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+# Supress FSADeprecationWarning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+
+
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:DJR8ul47p@localhost/bookings'
