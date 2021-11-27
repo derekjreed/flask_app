8480baac6f7521556d86e02fb15b66475e3333c7
Derek Reed <derekjreed@users.noreply.github.com>
2021-11-27 00:45:57 +0000

    Created prelim tests and an agency_test db backup

---
+++ projects/capstone/completed/agency_test.sql
@@ -0,0 +1,162 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.0
+-- Dumped by pg_dump version 14.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: Actor; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Actor" (
+    id integer NOT NULL,
+    name character varying(120) NOT NULL,
+    age integer NOT NULL,
+    gender character varying(7) NOT NULL
+);
+
+
+ALTER TABLE public."Actor" OWNER TO postgres;
+
+--
+-- Name: Actor_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."Actor_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public."Actor_id_seq" OWNER TO postgres;
+
+--
+-- Name: Actor_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."Actor_id_seq" OWNED BY public."Actor".id;
+
+
+--
+-- Name: Movie; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Movie" (
+    id integer NOT NULL,
+    title character varying(120) NOT NULL,
+    release_date timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public."Movie" OWNER TO postgres;
+
+--
+-- Name: Movie_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."Movie_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public."Movie_id_seq" OWNER TO postgres;
+
+--
+-- Name: Movie_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."Movie_id_seq" OWNED BY public."Movie".id;
+
+
+--
+-- Name: Actor id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Actor" ALTER COLUMN id SET DEFAULT nextval('public."Actor_id_seq"'::regclass);
+
+
+--
+-- Name: Movie id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Movie" ALTER COLUMN id SET DEFAULT nextval('public."Movie_id_seq"'::regclass);
+
+
+--
+-- Data for Name: Actor; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Actor" (id, name, age, gender) FROM stdin;
+1	Derek Rink	52	Male
+2	Jake Pound	32	Male
+3	Clarissa Hunt	38	Female
+\.
+
+
+--
+-- Data for Name: Movie; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Movie" (id, title, release_date) FROM stdin;
+1	Time to go	2022-01-01 00:00:00
+2	Time to come	2023-04-01 00:00:00
+\.
+
+
+--
+-- Name: Actor_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Actor_id_seq"', 3, true);
+
+
+--
+-- Name: Movie_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Movie_id_seq"', 3, true);
+
+
+--
+-- Name: Actor Actor_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Actor"
+    ADD CONSTRAINT "Actor_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: Movie Movie_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Movie"
+    ADD CONSTRAINT "Movie_pkey" PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- projects/capstone/completed/test_app.py
+++ projects/capstone/completed/test_app.py
@@ -1,1 +1,221 @@
+import json
+import os
+import unittest
+import time
 
+from flask_sqlalchemy import SQLAlchemy
+
+from app import create_app
+from models import (Actor, Movie, db_create_all,
+                    setup_db)
+
+
+class AgencyTestCase(unittest.TestCase):
+    """This class represents the agency test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "agency_test"
+        self.database_path = "postgresql://{}:{}@{}/{}".format(
+            'postgres', 'password123', 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+        db_create_all()
+
+        self.actor_correct_data = {
+            'name': 'James Don Senior',
+            'age': 58,
+            'gender': 'Male',
+        }
+
+        self.actor_partial_data = {
+            'name': 'James Don Junior',
+            'age': 28,
+        }
+
+        self.actor_no_data = {
+        }
+
+        self.movie_correct_data = {
+            'title': 'The long time',
+            'release_date': '2022-01-01'
+        }
+
+        self.movie_partial_data = {
+            'title': 'The best ever'
+        }
+
+        self.movie_no_data = {
+        }
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+# Actors
+
+    def test_1_get_paginated_actors(self):
+        res = self.client().get('/actors?page=1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.content_type, 'application/json')
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_actors'])
+        self.assertTrue(len(data['actors']))
+
+    def test_2_get_paginated_actors_page_not_found(self):
+        res = self.client().get('/actors?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.content_type, 'application/json')
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_8_delete_actors(self):
+        res = self.client().delete('/actors/1')
+        data = json.loads(res.data)
+
+        actor = Actor.query.filter(Actor.id == 1).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 1)
+        self.assertEqual(actor, None)
+
+    def test_4_delete_actors_beyond_valid_record(self):
+        res = self.client().delete('/actors/20')
+        data = json.loads(res.data)
+
+        actor = Actor.query.filter(Actor.id == 20).one_or_none()
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+        self.assertEqual(actor, None)
+
+    def test_5_post_actor_correct_data(self):
+        res = self.client().post('/actors', json=self.actor_correct_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertTrue(len(data['actors']))
+
+    def test_6_post_actor_missing_data(self):
+        res = self.client().post('/actors', json=self.actor_partial_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'], 'Unprocessable Entity')
+
+    def test_7_patch_actor_correct_data(self):
+        res = self.client().patch('/actors/1', json=self.actor_partial_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['actors'])
+
+    def test_3_patch_actor_no_data(self):
+        res = self.client().patch('/actors/1', json=self.actor_no_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'], 'Unprocessable Entity')
+
+# Movies
+
+    def test_9_get_paginated_movies(self):
+        res = self.client().get('/movies?page=1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.content_type, 'application/json')
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_movies'])
+        self.assertTrue(len(data['movies']))
+
+    def test_10_get_paginated_movies_page_not_found(self):
+        res = self.client().get('/movies?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.content_type, 'application/json')
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_15_delete_movies(self):
+        res = self.client().delete('/movies/1')
+        data = json.loads(res.data)
+
+        movie = Movie.query.filter(Movie.id == 1).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 1)
+        self.assertEqual(movie, None)
+
+    def test_12_delete_movies_beyond_valid_record(self):
+        res = self.client().delete('/movies/20')
+        data = json.loads(res.data)
+
+        movie = Movie.query.filter(Movie.id == 20).one_or_none()
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+        self.assertEqual(movie, None)
+
+    def test_13_post_movie_correct_data(self):
+        res = self.client().post('/movies', json=self.movie_correct_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertTrue(len(data['movies']))
+
+    def test_14_post_movie_missing_data(self):
+        res = self.client().post('/movies', json=self.movie_partial_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'], 'Unprocessable Entity')
+
+    def test_11_patch_movie_correct_data(self):
+        res = self.client().patch('/movies/1', json=self.movie_partial_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['movies'])
+
+    def test_16_patch_movie_no_data(self):
+        res = self.client().patch('/movies/1', json=self.movie_no_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'], 'Unprocessable Entity')
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
