1eb8006f9847f87a7a7562e19d50e6b5038a7bf0
Derek Reed <derekjreed@users.noreply.github.com>
2021-11-29 21:40:02 +0000

    Configured manage.py to run migrations and tested

--- projects/capstone/completed/manage.py
+++ projects/capstone/completed/manage.py
@@ -1,13 +1,18 @@
-from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app
+from flask_script import Manager
+
+from app import create_app, APP
 from models import db
 
-
-migrate = Migrate(app, db)
-manager = Manager(app)
+# Changed to uppercase APP as in app.py
+# use:
+# python manage.py db init
+# python manage.py db migrate
+# python manage.py db upgrade
+migrate = Migrate(APP, db)
+manager = Manager(APP)
 
 manager.add_command('db', MigrateCommand)
 
-if__name__ == '__main__':
-manager.run()
+if __name__ == '__main__':
+    manager.run()
