fd2265ae3b1d893fb8e865a7d9604141aeeb8c03
Derek Reed <derekjreed@users.noreply.github.com>
2021-11-30 22:10:15 +0000

    Added file as asked for in RUBRIC. All required configuration settings are included in a bash file which export:

---
+++ projects/capstone/completed/auth.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export AUTH0_DOMAIN='dev-ra41buva.eu.auth0.com'
+export AUTH0_CLIENT_ID='WFtfjIGVG35TNdnjlEiqDoQw1k7sWfF3'
+export AUTH0_CLIENT_SECRET='YRvx3XbwdDIdl10SiFmD2lXJzCt5EyW0n-RIvcQgakZPiMAIBHVVBmWrDAHIVlz2'
